9b54d580d0eb974eee7aa1ffa533233c6d5ee370
6284260 <111890803+6284260@users.noreply.github.com>
2022-09-02 09:56:34 -0500

    Update how_many_vowels.py

--- how_many_vowels.py
+++ how_many_vowels.py
@@ -9,31 +9,37 @@ def user_input():
   
     "Please input a word: "
  )
- 
+
   return txt
+  
 def count_vowels(txt):
+	int(count_vowels)
 	count_vowels=0
-	for char in txt:
+	for char in txt: 
 	  if "a" in txt(): 
-		  count_vowels+1
+	  	count_vowels+1
 	
-	for char in txt:
+	for char in txt: 
 		if "e" in txt():
-		 count_vowels+1
+			count_vowels+1
 	
-	for char in txt:
+	for char in txt: 
 		if "i" in txt():
-		 count_vowels+1
+			count_vowels+1
 	
-	for char in txt:
+	for char in txt: 
 		if "o" in txt():
-		 count_vowels+1
+			count_vowels+1
 	
-	for char in txt:
+	for char in txt: 
 		if "u" in txt():
-		 count_vowels+1
-	
+			count_vowels+1
+	return count_vowels
 def vowels(count_vowels):
-  count_vowels=vowels
-  return vowels
-  return vowels
+    vowels=count_vowels
+    print("this word has " + vowels)
+    return vowels
+    
+    
+txt=user_input()
+	
