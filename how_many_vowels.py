caedcfc557e42bda687701f0ee50bbaf3f4544aa
6284260 <111890803+6284260@users.noreply.github.com>
2022-09-07 09:36:17 -0500

    Update how_many_vowels.py

--- how_many_vowels.py
+++ how_many_vowels.py
@@ -1,45 +1,37 @@
-def user_input():
+def user_input(): 
   txt = input(
     """
     1.vowel check must use only one word at a time
-    2. word must not contain any special characters
+    2.word must not contain any special characters
     3.word must not contain numbers
   
     """
   
     "Please input a word: "
- )
-
-  return txt
+ ) 
   
+  return txt
+
 def count_vowels(txt):
-	int(count_vowels)
-	count_vowels=0
-	for char in txt: 
-	  if "a" in txt(): 
-	  	count_vowels+1
-	
-	for char in txt: 
-		if "e" in txt():
-			count_vowels+1
-	
-	for char in txt: 
-		if "i" in txt():
-			count_vowels+1
-	
-	for char in txt: 
-		if "o" in txt():
-			count_vowels+1
-	
-	for char in txt: 
-		if "u" in txt():
-			count_vowels+1
+  int(count_vowels)
+count_vowels=0
+  for char in txt: 
+  if "a" in txt():
+  	count_vowels=count_vowels + 1
+  if "e" in txt():
+  	count_vowels=count_vowels + 1
+  if "i" in txt():
+  	count_vowels=count_vowels + 1
+  if "o" in txt():
+  	count_vowels=count_vowels + 1
+  if "u" in txt():
+  	count_vowels=count_vowels + 1
+		 
 	return count_vowels
-def vowels(count_vowels):
-    vowels=count_vowels
-    print("this word has " + vowels)
-    return vowels
-    
-    
-txt=user_input()
 	
+ str(count_vowels)
+print("this word has " + count_vowels + " vowels")
+
+  
+
+txt=user_input()
