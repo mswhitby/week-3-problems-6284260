10711cf40b0e99dcb3b2c76a8cc1c0b3ead1bef8
6284260 <111890803+6284260@users.noreply.github.com>
2022-09-27 09:52:32 -0500

    Update calc_damage.py

--- calc_damage.py
+++ calc_damage.py
@@ -1,1 +1,62 @@
+def user_input():
+  input(
+    """
+    1.input damage value
+    2.damage cannot be negative
+    3.damage must be a number
+    
+    """
+    
+    "Please input damage: " 
+    
+  )
+  
+  return 
+  
 
+def user_inpt():
+  input(
+    """
+    1.input speed value
+    2.speed cannot be negative
+    3.speed must be a number
+    
+    """
+    
+    "Please input speed: " 
+    
+  )
+  
+  return 
+  
+def user_ipt():
+  input(
+    """
+    1.input time value
+    2.time cannot be negative
+    3.time must be a number
+    
+    """
+    
+    "Please input time: " 
+    
+  )
+  
+  return 
+  
+def damage_total():
+  DPS = damage * speed * time
+  for character in DPS:
+   if DPS < 0:
+    print("invalid")
+    break
+
+  return
+  print(str(DPS))
+  
+  
+  
+  
+damage = user_input()
+speed = user_inpt()
+time =  user_ipt()
